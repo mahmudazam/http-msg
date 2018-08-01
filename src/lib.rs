388b24fcb22854fcee7b15bc688337c669036822
Mahmud Azam <mfa256@mail.usask.ca>
2018-07-31 21:28:35 -0600

    Implement basic functions.
    - new
    - get_headers,
    - set_headers

--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,23 @@ mod tests {
     use response::Response;
 
     #[test]
-    fn test_req_parse() {
-        assert_eq!(2 + 2, 4);
+    fn test_req_create() {
+        let req = Request::new("GET", "/hello", "1.1");
+        assert_eq!(req.method, String::from("GET"));
+        assert_eq!(req.target, String::from("/hello"));
+        assert_eq!(req.version, String::from("HTTP/1.1"));
     }
 
     #[test]
-    fn test_res_parse() {
-        assert_eq!(2 + 2, 4);
+    fn test_req_get_set_header() {
+        let name = "Content-Type";
+        let value = "text/html";
+        let mut req = Request::new("GET", "/hello", "1.1");
+        assert_eq!(req.get_header(name), None);
+        assert_eq!(req.set_header(name, value)
+                      .get_header(name),
+                   Some(&String::from(value)));
     }
+
 }
+
